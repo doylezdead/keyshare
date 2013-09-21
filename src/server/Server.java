ab6cc0021568b84b36f0c7ee76bf7d08e1499ce5
Ryan Doyle <rcdoyle@mtu.edu>
2013-09-21 01:39:38 -0400

    added server stuffs

---
+++ src/server/Server.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	public static void main(String[] args){
+		String data = "This is test data";
+		try {
+			ServerSocket srvr = new ServerSocket(1234);
+			Socket skt = srvr.accept();
+			System.out.print("Server has connected!\n");
+			PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+			System.out.print("Sending string: '" + data + "'\n");
+			out.print(data);
+			out.close();
+			skt.close();
+			srvr.close();
+		}
+		catch(Exception e) {
+			System.out.print("Whoops! It didn't work!\n");
+		}
+	}
+}
