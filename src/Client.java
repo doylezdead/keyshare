b1854e6df413e86d7000aaab6bca7d7f268a5f80
Ryan Doyle <rcdoyle@mtu.edu>
2013-09-21 01:14:34 -0400

    test commit

---
+++ src/Client.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author Ryan Doyle, Sven Stroven, Chris Coppernoll
+ * 
+ *
+ */
+public class Client {
+	public static void main(String[] args){
+		System.out.println("test");
+	}
+
+}
